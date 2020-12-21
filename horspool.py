93ba57de6ff23c0f66be811f3d6b2eb148410fab
ahmetbuckoUP <75639075+ahmetbuckoUP@users.noreply.github.com>
2020-12-21 20:01:55 +0100

    Implemented Dict with 5 samples.

--- horspool.py
+++ horspool.py
@@ -3,6 +3,7 @@ def horspool(text,pattern):
     i = 0
     m = len(pattern)
     n = len(text)
+    print('Texti : "{}" Paterni: "{}"'.format(text,pattern))
 
     while i <= n - m:
         j = m - 1
@@ -26,48 +27,17 @@ def horspool(text,pattern):
 
         i = i - occ
 
-    print('Found', found, 'matches')
+    print('Patterni u gjet {} here'.format(found))
+
+data = {
+    'asfsafasaahmetasdadasd':'ahmet',
+    'saahmetas':'ahmet',
+    'asdasmetasahmetdasdahmet':'ahmet',
+    'Ne shikojme filma bashke me ahmetin':'ahmet',
+    'datastring':'ahmet',
+}
 
 
-text = ""
-pattern = ""
+for x in data:
+    horspool(x,data[x])
 
-while len(text) <= 0:
-    text = input("Enter text: ")
-
-while len(pattern) <= 0:
-    pattern = input("Enter pattern: ")
-
-    horspool(text,pattern)
-
-
-
-# Init Data for Horspool
-# found = 0
-# i = 0
-# m = len(pattern)
-# n = len(text)
-#
-# while i <= n - m:
-#     j = m - 1
-#
-#     while j >= 0 and pattern[j] == text[i + j]:
-#         j = j - 1
-#
-#     if j < 0:
-#         found = found + 1
-#
-#     i = i + m - 1
-#
-#     occ = m - 2
-#
-#     while occ >= 0:
-#         if pattern[occ] == text[i]:
-#             # print(pattern[occ])
-#             # print(text[i])
-#             break
-#         occ = occ - 1
-#
-#     i = i - occ
-#
-# print('Found', found, 'matches')
