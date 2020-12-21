c019894eb28c2d28aeacf4e159e7352435a100c1
ahmetbuckoUP <75639075+ahmetbuckoUP@users.noreply.github.com>
2020-12-21 19:50:54 +0100

    Def Function Implemented

--- horspool.py
+++ horspool.py
@@ -1,3 +1,33 @@
+def horspool(text,pattern):
+    found = 0
+    i = 0
+    m = len(pattern)
+    n = len(text)
+
+    while i <= n - m:
+        j = m - 1
+
+        while j >= 0 and pattern[j] == text[i + j]:
+            j = j - 1
+
+        if j < 0:
+            found = found + 1
+
+        i = i + m - 1
+
+        occ = m - 2
+
+        while occ >= 0:
+            if pattern[occ] == text[i]:
+                # print(pattern[occ])
+                # print(text[i])
+                break
+            occ = occ - 1
+
+        i = i - occ
+
+    print('Found', found, 'matches')
+
 
 text = ""
 pattern = ""
@@ -8,32 +38,36 @@ while len(text) <= 0:
 while len(pattern) <= 0:
     pattern = input("Enter pattern: ")
 
+    horspool(text,pattern)
+
+
+
 # Init Data for Horspool
-found = 0
-i = 0
-m = len(pattern)
-n = len(text)
-
-while i <= n - m:
-    j = m - 1
-
-    while j >= 0 and pattern[j] == text[i + j]:
-        j = j - 1
-
-    if j < 0:
-        found = found + 1
-
-    i = i + m - 1
-
-    occ = m - 2
-
-    while occ >= 0:
-        if pattern[occ] == text[i]:
-            # print(pattern[occ])
-            # print(text[i])
-            break
-        occ = occ - 1
-
-    i = i - occ
-
-print('Found', found, 'matches')
+# found = 0
+# i = 0
+# m = len(pattern)
+# n = len(text)
+#
+# while i <= n - m:
+#     j = m - 1
+#
+#     while j >= 0 and pattern[j] == text[i + j]:
+#         j = j - 1
+#
+#     if j < 0:
+#         found = found + 1
+#
+#     i = i + m - 1
+#
+#     occ = m - 2
+#
+#     while occ >= 0:
+#         if pattern[occ] == text[i]:
+#             # print(pattern[occ])
+#             # print(text[i])
+#             break
+#         occ = occ - 1
+#
+#     i = i - occ
+#
+# print('Found', found, 'matches')
